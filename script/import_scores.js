92186f07363bafa92cc6e3dcb7bd3e77c23387b0
Andrew Matheny <andrew.matheny@listhub.com>
2013-02-23 16:06:01 -0500

    import scores into mongo

--- script/import_scores.js
+++ script/import_scores.js
@@ -1,8 +1,18 @@
 var fs  = require("fs")
 var csv = require("csv")
 
-var header = []
+// Mongo configuration parameters
+var MONGO_ROOT_URL = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/';
+var MONGO_URL = MONGO_ROOT_URL + 'movepgh';
 
+// Setup mongoose and the facet model
+var mongoose = require("mongoose");
+var facet_model = require("../model/facet_model");
+
+// Connect to mongo
+mongoose.connect(MONGO_URL);
+
+var header = []
 csv().from.stream(fs.createReadStream("script/csv/move-to-pitt_data - Scores.csv"))
 .transform( function(row, index){
 	if (index === 0) {
@@ -15,7 +25,15 @@ csv().from.stream(fs.createReadStream("script/csv/move-to-pitt_data - Scores.csv
 	row.forEach(function(hood_score, index){
 		if (index === 0) return
 		scores[header[index]] = parseFloat(hood_score)
-  	})
-	
-	console.log(JSON.stringify({facet_id: facet_id, scores: scores}))
+  })
+	console.log(JSON.stringify({id: facet_id, neighborhood_scores: scores}))
+  
+  facet_model.findOneAndUpdate({id: facet_id}, {neighborhood_scores: scores}, {upsert: true}, function updateHandler(err) {
+    if (err) {
+      console.log("Unable to update neighborhood scores for facet. " + err);
+      retun;
+    } else {
+      console.log("Neighborhood scores for " + facet_id + " updated");
+    }
+  })
 });
