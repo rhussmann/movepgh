8060c113e36b04693b96464b61d41c1ebced5f3a
Andrew Matheny <andrew.matheny@listhub.com>
2013-02-23 15:32:56 -0500

    Neighborhood model and import

---
+++ model/neighborhood_model.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+
+var hood_schema = mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true }
+});
+
+module.exports = mongoose.model('neighborhood', hood_schema);
--- script/import_hoods.js
+++ script/import_hoods.js
@@ -1,6 +1,17 @@
 var fs  = require("fs")
 var csv = require("csv")
 
+// Mongo configuration parameters
+var MONGO_ROOT_URL = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/';
+var MONGO_URL = MONGO_ROOT_URL + 'movepgh';
+
+// Setup mongoose and the facet model
+var mongoose = require("mongoose");
+var facet_model = require("../model/facet_model");
+
+// Connect to mongo
+mongoose.connect(MONGO_URL);
+
 csv().from.stream(fs.createReadStream("script/csv/move-to-pitt_data - Neighborhoods.csv"))
 .transform( function(row, index){
 	if (index === 0) return
@@ -8,6 +19,15 @@ csv().from.stream(fs.createReadStream("script/csv/move-to-pitt_data - Neighborho
 	neighborhood.id = row[0]
 	neighborhood.name = row[1]
 	neighborhood.description = row[2]
-	console.log(JSON.stringify(neighborhood))
+  console.log(JSON.stringify(neighborhood))
+
+  facet_model.findOneAndUpdate({id: neighborhood.id}, neighborhood, {upsert: true}, function updateHandler(err) {
+    if (err) {
+      console.log("Unable to update neighborhood. " + err);
+      retun;
+    } else {
+      console.log("neighborhood " + neighborhood.id + " updated");
+    }
+  })
 });
 
