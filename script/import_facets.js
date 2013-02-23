d526d849a3fa951c62c20c2536aebf957e567518
Ricky Hussmann <ricky.hussmann@gmail.com>
2013-02-23 16:21:12 -0500

    Added async and converted import_facets to be async-compatible

--- script/import_facets.js
+++ script/import_facets.js
@@ -1,5 +1,6 @@
-var fs  = require("fs")
-var csv = require("csv")
+var async = require("async")
+var fs    = require("fs")
+var csv   = require("csv")
 
 // Mongo configuration parameters
 var MONGO_ROOT_URL = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/';
@@ -12,6 +13,8 @@ var facet_model = require("../model/facet_model");
 // Connect to mongo
 mongoose.connect(MONGO_URL);
 
+var facet_update_tasks = [];
+
 csv().from.stream(fs.createReadStream("script/csv/move-to-pitt_data - Facets.csv"))
 .transform( function(row, index){
   if (index === 0) return
@@ -22,12 +25,38 @@ csv().from.stream(fs.createReadStream("script/csv/move-to-pitt_data - Facets.csv
   facet.category = row[3]
   console.log(JSON.stringify(facet))
 
-  facet_model.findOneAndUpdate({id: facet.id}, facet, {upsert: true}, function updateHandler(err) {
+  // Add an async task for upserting this facet
+  facet_update_tasks.push(createAsyncMongooseUpdateFunction(facet));
+})
+
+// Finished parsing the CSV, fire off the update tasks
+.on("end", function executeUpdates() {
+  async.parallel(facet_update_tasks, function(err, results) {
     if (err) {
-      console.log("Unable to update facet. " + err);
-      retun;
+      console.log("Error updating facets: " + err);
     } else {
-      console.log("Facet " + facet.id + " updated");
+      console.log("Successfully updated facets");
     }
-  })
-});
+    disconnectMongoose();
+  });
+})
+
+// Error parsing CSV, bail
+.on("error", disconnectMongoose);
+
+function createAsyncMongooseUpdateFunction(facet) {
+  return function mongooseUpdateFacet(callback) {
+    facet_model.findOneAndUpdate({id: facet.id}, facet, {upsert: true}, function updateHandler(err) {
+      if (err) {
+        return callback(err);
+      } else {
+        console.log("Facet " + facet.id + " updated");
+        return callback(null);
+      }
+    });
+  }
+}
+
+function disconnectMongoose () {
+  mongoose.disconnect();
+}
